346a62066299bf38e1458747eb167f9bf826226b
Alam-Ashraf <alamgeer06@gmail.com>
2021-02-20 11:10:43 +0530

    Add Biometric Functionality

--- XFBiometric.Android/MainActivity.cs
+++ XFBiometric.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Plugin.Fingerprint;
 
 namespace XFBiometric.Droid
 {
@@ -20,6 +21,8 @@ namespace XFBiometric.Droid
             base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            CrossFingerprint.SetCurrentActivityResolver(() => Xamarin.Essentials.Platform.CurrentActivity);
+
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
--- XFBiometric/MainPage.xaml.cs
+++ XFBiometric/MainPage.xaml.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Plugin.Fingerprint;
+using Plugin.Fingerprint.Abstractions;
 using Xamarin.Forms;
 
 namespace XFBiometric
@@ -13,6 +15,40 @@ namespace XFBiometric
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        async void OnAuthenticateClicked(System.Object sender, System.EventArgs e)
+        {
+            var availability = await CrossFingerprint.Current.IsAvailableAsync();
+
+            if (!availability)
+            {
+                StackContainer.IsVisible = false;
+                await DisplayAlert("Warning!", "No biometrics available", "OK");
+
+                return;
+            }
+
+            var authResult = await CrossFingerprint.Current.AuthenticateAsync(new AuthenticationRequestConfiguration("Heads up!", "Please validate biometric !!"));
+
+            if (authResult.Authenticated)
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    StackContainer.IsVisible = true;
+                    ImgSuccess.Source = ImageSource.FromFile("success.png");
+                    LblMsg.Text = "Authentication Successfully !!";
+                });
+            }
+            else
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    StackContainer.IsVisible = true;
+                    ImgSuccess.Source = ImageSource.FromFile("cross.jpg");
+                    LblMsg.Text = "Authentication Failed !!";
+                });
+            }
         }
     }
 }
